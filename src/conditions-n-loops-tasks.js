c486671988b9e85bd8c0f7a123f6c9ed407a2b35
Olha Teplova <olitieplove@gmail.com>
2024-01-15 20:18:41 +0200

    feat: implement 10 tests

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -375,8 +375,16 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  for (let i = 1; i < arr.length; i += 1) {
+    const currentEl = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentEl; j -= 1) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentEl;
+  }
+  return arr;
 }
 
 /**
